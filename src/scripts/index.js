2333f145735a1be4bbc6ac98a04b0ee205014971
Samaria Valdez <63667009+Samariavs@users.noreply.github.com>
2021-05-21 17:13:04 -0400

    I rename the constants

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
-const hourToShow = document.getElementById('hour');
-const minuteToShow = document.getElementById('minute');
-const secondsToShow = document.getElementById('second');
-const meridianIndicatorToShow = document.getElementById('meridianIndicator');
+const hoursElement= document.getElementById('hour');
+const minutesElement = document.getElementById('minute');
+const secondsElement = document.getElementById('second');
+const meridianIndicatorElement = document.getElementById('meridianIndicator');
 
 const clock = () =>{
 
@@ -13,10 +13,10 @@ const clock = () =>{
         let seconds = clock.getSeconds();
 
        
-        hourToShow.textContent = (hours > 12)? hours - 12 : hours;
-        minuteToShow.textContent = (minutes < 10)? `0${minutes}`: minutes;
-        secondsToShow.textContent = (seconds <10)? `0${seconds}`: seconds;
-        meridianIndicatorToShow.textContent = (hours > 12)? 'PM': 'AM';
+        hoursElement.textContent = (hours > 12)? hours - 12 : hours;
+        minutesElement.textContent = (minutes < 10)? `0${minutes}`: minutes;
+        secondsElement.textContent = (seconds <10)? `0${seconds}`: seconds;
+        meridianIndicatorElement.textContent = (hours > 12)? 'PM': 'AM';
     };
 
   showClock();
