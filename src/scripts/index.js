0f825f9e6a1befe3a8ba504209eab70a0b58f8af
Samaria Valdez <63667009+Samariavs@users.noreply.github.com>
2021-05-21 17:08:49 -0400

    add literal templates and shorten code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,23 +10,13 @@ const clock = () =>{
         const clock = new Date();
         let hours = clock.getHours();
         let minutes = clock.getMinutes();
-        let period;
         let seconds = clock.getSeconds();
 
-        period = (hours > 12)? 'PM' : 'AM';
-        hours = (hours > 12)? hours - 12 : hours;
-
-        if(minutes < 10){
-           '0 ${minutes';
-        }
-
-        if(seconds < 10){
-           '0 ${seconds}'
-        }
-        hourToShow.textContent = hours;
-        minuteToShow.textContent = minutes;
-        secondsToShow.textContent = seconds;
-        meridianIndicatorToShow.textContent = period;
+       
+        hourToShow.textContent = (hours > 12)? hours - 12 : hours;
+        minuteToShow.textContent = (minutes < 10)? `0${minutes}`: minutes;
+        secondsToShow.textContent = (seconds <10)? `0${seconds}`: seconds;
+        meridianIndicatorToShow.textContent = (hours > 12)? 'PM': 'AM';
     };
 
   showClock();
